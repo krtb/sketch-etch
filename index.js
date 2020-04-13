f6059df54034ffe10a3b3ae0c23ca863694455d0
Kurt Bauer <kurtbauer55@protonmail.com>
2020-04-13 17:48:38 -0400

    grab shake button with querySelector

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+// get Canvas Context from div
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
+
+// Grab shake button
+const shakeButton = document.querySelector('.shake');
