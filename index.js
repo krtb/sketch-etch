ac7ea6ddc434f70a56e6cfdf495f4036bb88d2d3
Kurt Bauer <kurtbauer55@protonmail.com>
2020-04-13 17:54:14 -0400

    place the drawing on canvas

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ const shakeButton = document.querySelector('.shake');
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 10;
+
+ctx.beginPath(); // start the drawing
+ctx.moveTo(200, 200);
+ctx.lineTo(200, 200);
+ctx.stroke();
